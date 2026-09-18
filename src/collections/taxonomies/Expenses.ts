3b8c57d383d189451ba0b5d393b0672c9dea28b3
Bagas Ranggen <bagas.ranggen@thepixelage.com>
2026-09-18 16:15:16 +0700

    WIP: update expense collection

--- src/collections/taxonomies/Expenses.ts
+++ src/collections/taxonomies/Expenses.ts
@@ -1,5 +1,31 @@
-import { CollectionConfig } from 'payload';
+import { CollectionConfig, Option } from 'payload';
+
+import { ArrayStringProps } from '@/libs/types';
+import { joinArrayString } from '@/libs/utils';
+
 import { BaseEntry } from '@/shared';
+
+export const EXPENSE_TYPE_OPTIONS: Exclude<Option, string>[] = [
+    {
+        value: 'crew',
+        label: 'Crew',
+    },
+    {
+        value: 'etc',
+        label: 'Et Cetera',
+    },
+];
+
+export const CREW_ROLES_OPTIONS: Exclude<Option, string>[] = [
+    {
+        value: 'soundEngineer',
+        label: 'Sound Engineer',
+    },
+    {
+        value: 'documentation',
+        label: 'Documentation',
+    },
+];
 
 export const Expenses: CollectionConfig = {
     slug: 'expenses',
@@ -9,8 +35,39 @@ export const Expenses: CollectionConfig = {
     },
     fields: BaseEntry({
         typeHandle: [{ value: 'sectionExpense', label: 'Expense' }],
-        url: {
-            enabled: false,
+        url: { enabled: false },
+        general: {
+            title: {
+                admin: {
+                    readOnly: true,
+                },
+                hooks: {
+                    beforeChange: [
+                        async ({ siblingData, req: { payload } }) => {
+                            let data: ArrayStringProps = [];
+
+                            const expenseType = EXPENSE_TYPE_OPTIONS.find(
+                                (item) => item?.value === siblingData?.expenseType
+                            );
+
+                            if (siblingData?.expenseType === 'crew') {
+                                if (expenseType && expenseType?.label) data.push(expenseType.label as string);
+
+                                try {
+                                    const crew = await payload.findByID({
+                                        collection: 'people',
+                                        id: siblingData?.crew,
+                                    });
+
+                                    if (crew?.title) data.push(crew?.title as string);
+                                } catch (e) {}
+                            }
+
+                            return joinArrayString(data, ' - ');
+                        },
+                    ],
+                },
+            },
         },
         tabs: [
             {
@@ -33,16 +90,7 @@ export const Expenses: CollectionConfig = {
                         type: 'select',
                         name: 'expenseType',
                         label: 'Type',
-                        options: [
-                            {
-                                value: 'crew',
-                                label: 'Crew',
-                            },
-                            {
-                                value: 'etc',
-                                label: 'Et Cetera',
-                            },
-                        ],
+                        options: EXPENSE_TYPE_OPTIONS,
                     },
                     {
                         type: 'relationship',
@@ -71,16 +119,7 @@ export const Expenses: CollectionConfig = {
                                     width: '40%',
                                     condition: (data, siblingData) => siblingData?.expenseType === 'crew',
                                 },
-                                options: [
-                                    {
-                                        value: 'soundEngineer',
-                                        label: 'Sound Engineer',
-                                    },
-                                    {
-                                        value: 'documentation',
-                                        label: 'Documentation',
-                                    },
-                                ],
+                                options: CREW_ROLES_OPTIONS,
                             },
                         ],
                     },
